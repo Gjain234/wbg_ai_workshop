e04704f76797acb84110532d564933912df4a8be
Gauri Jain <Gauri Jain>
2026-05-02 01:32:28 -0400

    worksheet

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+# Route for AI Tool Picker tab
+@app.route("/ai-tool-picker")
+def ai_tool_picker():
+    return render_template("ai-tool-picker.html")
